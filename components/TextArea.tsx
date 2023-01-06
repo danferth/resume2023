31c8b38726fda93a0da02e1483aaf1046a58b684
danferth <danferth@gmail.com>
2023-01-05 17:24:34 -0800

    forgot this one lol

--- components/TextArea.tsx
+++ components/TextArea.tsx
@@ -5,7 +5,14 @@ const TextArea = (props: {
   placeholder: string;
 }) => {
   return (
-    <div className="relative rounded-md border border-stone-300 px-3 py-2 shadow-sm focus-within:border-amber-600 focus-within:ring-1 focus-within:ring-amber-600">
+    <div
+      className="relative 
+    rounded-md 
+    border border-stone-300 
+    px-3 py-2 
+    shadow-sm 
+    focus-within:border-amber-600 focus-within:ring-1 focus-within:ring-amber-600"
+    >
       <label
         htmlFor={props.id}
         className="absolute -top-2 left-2 -mt-px inline-block bg-white px-1 text-xs font-medium text-stone-900"
